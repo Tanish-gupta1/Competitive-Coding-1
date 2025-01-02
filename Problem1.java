dff316d4dccd52eb3bb2a7fa7e7708457aa48ea6
tanish-gupta1 <tanishpargal098765@gmail.com>
2025-01-02 19:04:20 +0530

    Competitive-Coding-1 :  Problem 1 and Problem 2 completed.

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,39 @@
+//Given a list of n-1 integers and these integers are in the range of 1 to n. There are no duplicates in list. One of the integers is missing in the list. Write an efficient code to find the missing integer.
+//        Examples:
+//
+//Input : arr[] = [1, 2, 3, 4, 6, 7, 8]
+//Output : 5
+//
+//
+//Input : arr[] = [1, 2, 3, 4, 5, 6, 8, 9]
+//Output : 7
 
+
+//approach is to use tweaked binarySearch
+//the base case here is that if we want to find missing number between 2 no ->  [5,7] ->5+7/2->6 this will be our result base case
+class Problem1 {
+    public static int search(int[] arr){
+        int low = 0;
+        int high = arr.length-1;
+        while((high-low)>=2){//because we want to reach a point where there are 2 elements only left where we know the missing number lies between them
+            int mid = low +(high-low)/2;
+            int lowDiff = arr[low]-low;
+            int highDiff = arr[high] - high;
+            int midDiff = arr[mid]-mid;
+
+            if(lowDiff!=midDiff){
+                high = mid;
+            }
+            else if(midDiff != highDiff){
+                low = mid;
+            }
+        }
+        return (arr[low]+arr[high])/2;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5,6,7};
+        System.out.println("missing number :"+search(arr));
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,100 @@
 
+public class Problem2 {
+    public class Heap{
+        int[] heap;
+        int capacity;
+        int size;
+
+        Heap{
+            size = 0;
+            capacity = 10;
+            heap = new int[capacity];
+            heap[size]=Math.min;
+        }
+
+        //T.c - 0(1)
+        public int findParentIndex(int index){
+            return index/2;
+        }
+        //T.c - 0(1)
+        public int leftChildIndex(int index){
+            return index*2;
+        }
+        //T.c - 0(1)
+        public int rightChildIndex(int index){
+            return index*2+1;
+        }
+        //T.c - 0(1)
+        public boolean isLeafNode(int index){
+            return size/2 < index;
+        }
+        //T.c - 0(1)
+        public int size(){
+            return size;
+        }
+        //T.c - 0(1)
+        public int peek(){
+            return heap[1];
+        }
+
+        public void swap(int fPos, int sPos){
+            int temp = heap[fPos];
+            heap[fPos] = heap[sPos];
+            heap[sPos] = temp;
+        }
+        //T.c - 0(logN) -> we're going from bottom to top
+        public void add(int element){
+            if(size>= capacity){
+                System.out.println("No space, can't add any more element");
+                return;
+            }
+            size++;
+            heap[size] = element;
+            int currIndex = size;
+
+            //check if the current index element is smaller than its parent index element;
+            while(heap[currIndex] < heap[findParentIndex(curr)]){
+                //swap them
+                swap(currIndex, findParentIndex(currIndex));
+                // change the current to parent index;
+                currIndex = findParentIndex(currIndex);
+            }
+        }
+        //T.c - 0(logN)-> we're going from top to bottom
+        public void heapiFy(int index){
+            if(!isLeafNode(index)){//we should not be at the leaf
+                // check if the left child or right clild is smaller
+                if(heap[index]>heap[leftChildIndex(index)] ||  heap[index] > heap[rightChildIndex(index)]){
+                    //if right is smaller
+                    if(heap[rightChildIndex(index)]<heap[index]){
+                        //swap
+                        swap(index,rightChildIndex(index));
+                        //change the current index
+                        index = rightChildIndex(index);
+                        //recursive call
+                        heapiFy(index);
+                    }
+                    else{
+                        swap(index,leftChildIndex(index));
+                        index = leftChildIndex(index);
+                        heapiFy(index);
+                    }
+                }
+            }
+        }
+        //T.c - 0(logN)
+        public int remove(){
+            int removedElement = heap[1];
+            heap[1] =  heap[size];
+            size--;
+            heapiFy(1);
+
+            return removedElement;
+        }
+
+    }
+    public static void main(String[] args) {
+        Heap minHeap = new Heap();
+        System.out.println("Min Heap Impl : ");
+    }
+}
